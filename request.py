e915407d7584b1804b5b6f46bc64d3b850cb0186
farshad <farshadnp6@gmail.com>
2021-08-12 01:45:52 +0430

    1.Bug Fix. 2.Get more picture using scrolling.

--- request.py
+++ request.py
@@ -6,6 +6,7 @@ import pandas as pd
 from info import user, passwd
 import time
 
+
 class Bot():
     def __init__(self):
         self.login(user, passwd)
@@ -14,50 +15,62 @@ class Bot():
         self.driver = webdriver.Firefox()
         self.driver.get('https://www.instagram.com/')
 
-        sleep(10)   
+        sleep(10)
         username_input = self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input')
         username_input.send_keys(username)
+        print(">>>> Username inserted")
         sleep(1)
-        
+
         password_input = self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')
         password_input.send_keys(password)
+        print(">>>> Password inserted")
         sleep(1)
 
         # click on Login-button
         self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button/div').click()
+        print(">>>> Loged in")
         sleep(7)
 
-        #redirect to main panel of instagram
-        #self.driver.get('https://www.instagram.com/')
-        #sleep(4)
+        # redirect to main panel of instagram
+        # self.driver.get('https://www.instagram.com/')
+        # sleep(4)
 
-        #Bypass save your login info popup
-        #self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
-        #sleep(4)
+        # Bypass save your login info popup
+        # self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
+        # sleep(4)
 
     # go to #BTC hashtah with 7billion post over the world
         self.driver.get('https://www.instagram.com/explore/tags/btc/')
         sleep(6)
-        
-        # find image src with loop condition   
+
+        # scroll page for show more post (bypass post lazy loading)
+        print(">>>> Scrolling for Get more post...")
+        for i in range(300):
+            self.driver.execute_script("window.scrollBy(0, 400)")
+            sleep(0.8)
+
+        # find image src with loop condition
         listOfSrc = []
         for i in self.driver.find_elements_by_class_name('FFVAD'):
             listOfSrc.append(i.get_attribute('src'))
-            print(listOfSrc)
-            print('__________________________\n')
-            sleep(0.2)
-            
-            #scroll page for show more post (bypass post lazy loading)
-            self.driver.execute_script("window.scrollBy(0, 250)")
-            time.sleep(0.5)
+            sleep(1)
+            #print(listOfSrc)
+            #print('__________________________\n')
+            #sleep(0.1)
+            # make dataframe from src tags
+            UrlDF = pd.DataFrame(listOfSrc)
+            # export src as CSV file for next steps
+            UrlDF.to_csv('url.csv')
+
 
         # make dataframe from src tags
-        UrlDF = pd.DataFrame(listOfSrc) 
-        #export src as CSV file for next steps
+        UrlDF = pd.DataFrame(listOfSrc)
+        # export src as CSV file for next steps
         UrlDF.to_csv('url.csv')
+
 
 def main():
     myBot = Bot()
